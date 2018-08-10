3921fb96c7976e4f8145a419daedf4364e47fd1c
Jordan Marques <jordanmarques1994@gmail.com>
2018-08-10 11:44:51 +0200

    add logs

--- status-menu-bar.js
+++ status-menu-bar.js
@@ -1,4 +1,6 @@
 
+require("babel-polyfill")
+var log = require('electron-log');
 const path = require('path')
 const menubar = require('menubar')
 const fetch = require('electron-fetch').default
@@ -7,35 +9,39 @@ const DOMParser = require('xmldom').DOMParser;
 var mb = menubar()
 
 mb.on('ready', () => {
-	callAsyncTraficJamFetch()
+	log.info('App is ready');
+	fetchTraficJam().then(displayTraficJam)
 	setInterval(() => {
-		callAsyncTraficJamFetch()
+		fetchTraficJam().then(displayTraficJam)
 	}, 60000)
 	
 })
 
+const fetchTraficJam = () => {
+	log.info('Start fetch');
+	return fetch('http://www.sytadin.fr', {headers: {Accept: 'application/json'}})
+		.then(toString)
+    	.then(extractTraficJam)
+}
+
+const toString = (res) => {
+	log.info('toString');
+	return res.text()
+}
+
 const extractTraficJam = (body) => {
+	log.info('extract');
 	var parser = new DOMParser()
 	var doc = parser.parseFromString(body, "text/html")
 	var childs = doc.getElementById("cumul_bouchon").childNodes
 	var jam = childs[3].getAttribute("alt")
 
 	jam = jam.replace(" km", "")
+	log.info('jam: ' + jam);
 
 	return new Promise(resolve => resolve(jam))
 }
 
-const toString = (res) => {
-	return res.text()
-}
-
 const displayTraficJam = (jam) => {
 	mb.tray.setTitle(jam + " Km")
-}
-
-const callAsyncTraficJamFetch = () => {
-	fetch('http://www.sytadin.fr')
-		.then(toString)
-    	.then(extractTraficJam)
-    	.then(displayTraficJam)
 }
